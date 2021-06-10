fada9395b395b1dacd6094ee9b53103977f8c6d7
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-10 03:02:03 -0500

    Create Command.rb

---
+++ Command.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Bcu
+  class Command
+    class CommandNotImplementedError < NoMethodError
+    end
+
+    def process(_args, _options)
+      raise Command::CommandNotImplementedError, "#{self.class.name} needs to implement 'process' method!"
+    end
+  end
+end
