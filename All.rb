1da9ba56a7b52768857d2ddfe86bd60f59054dd6
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-10 03:02:52 -0500

    Create All.rb

---
+++ All.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "#{File.dirname(__FILE__)}/command"
+require "#{File.dirname(__FILE__)}/pin_add"
+require "#{File.dirname(__FILE__)}/pin_list"
+require "#{File.dirname(__FILE__)}/pin_remove"
+require "#{File.dirname(__FILE__)}/upgrade"
