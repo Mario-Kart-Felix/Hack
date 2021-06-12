1d40d830154e3532e503d5698f2d306640697de6
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-12 14:34:34 -0500

    Create Component.hs

---
+++ Component.hs
@@ -0,0 +1,27 @@
+{
+    "name": "mixing",
+    "repository": "gamtiq/mixing",
+    "description": "Functions to mix, filter, change and copy/clone objects",
+    "version": "1.3.0",
+    "keywords": [
+        "mix",
+        "merge",
+        "mixin",
+        "assign",
+        "object",
+        "filter",
+        "map",
+        "change",
+        "copy",
+        "clone",
+        "modify",
+        "update"
+    ],
+    "dependencies": {},
+    "development": {},
+    "license": "MIT",
+    "main": "index.js",
+    "scripts": [
+        "index.js"
+    ]
+}
