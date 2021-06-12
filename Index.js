a5bcad148ecb7e04a29cf2ae015c6864d0d59d6d
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-12 14:31:56 -0500

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,29 @@
+{
+    "source": {
+        "include": ["index.js"]
+    },
+
+    "opts": {
+        "destination": "docs",
+        "readme": "README.md",
+        "template": "node_modules/ink-docstrap/template",
+        "fileSet": ".nojekyll"
+    },
+
+    "plugins": [
+        "plugins/markdown",
+        "jsdoc-file"
+    ],
+
+    "markdown": {
+        "parser": "marked"
+    },
+    
+    "templates": {
+        "outputSourceFiles": true,
+        "systemName": "mixing",
+        "copyright": "Copyright (c) 2013-2021 Denis Sikuler",
+        "theme": "spacelab",
+        "linenums": true
+    }
+}
