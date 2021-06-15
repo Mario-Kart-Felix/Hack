d3cbad51f5fe5270279b35a24df573cea6d7ccbb
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-15 15:30:05 -0500

    Create Inveigh.psm1

---
+++ Inveigh.psm1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Inveigh is a Os Project PowerShell ADIDNS/LLMNR/mDNS/NBNS spoofer/man-in-the-middle tool.
+
+.LINK
+https://github.com/Mario-Kart-Felix/Inveigh
+#>
+Import-Module $PWD\Inveigh.ps1
+Import-Module $PWD\Inveigh-Relay.ps1
